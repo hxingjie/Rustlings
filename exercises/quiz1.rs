e117a7ff55019a9ddaf692ef9e30e4789efbe70a
ubuntu <ubuntu@localhost.localdomain>
2025-01-16 16:01:00 +0800

    finish pre-exercise

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(mount: u32) -> u32 {
+    if (mount <= 40) {
+        mount * 2
+    } else {
+        mount
+    }
+}
 
 // Don't modify this function!
 #[test]
